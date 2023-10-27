03831cb5a5a50a414d340c1137a73d26f6236513
MengJingsong <MengJS.92@gmail.com>
2023-10-27 11:15:24 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,27 @@ source /users/jason92/.bashrc
 
 cd /users/jason92/local/hadoop-3.3.6/etc/hadoop
 
+sudo cat >> core-site-customize << EOF
+  <property>
+    <name>fs.defaultFS</name>
+    <value>hdfs://namenode:9000</value>
+  </property>
+EOF
+sudo sed -i '/<configuration>/r core-site-customize' core-site.xml
+
 sudo cat >> hdfs-site-customize << EOF
+  <property>
+    <name>dfs.namenode.name.dir</name>
+    <value>/users/jason92/hadoop/data/namenode</value>
+  </property>
+  <property>
+    <name>dfs.namenode.data.dir</name>
+    <value>/users/jason92/hadoop/data/datanode</value>
+  </property>
+  <property>
+    <name>dfs.replication</name>
+    <value>1</value>
+  </property>
   <property>
     <name>dfs.namenode.http-address</name>
     <value>127.0.0.1:50070</value>
@@ -26,8 +46,32 @@ sudo sed -i '/<configuration>/r hdfs-site-customize' hdfs-site.xml
 
 sudo cat >> yarn-site-customize << EOF
   <property>
+    <name>yarn.acl.enable</name>
+    <value>0</value>
+  </property>
+  <property>
+    <name>yarn.resourcemanager.hostname</name>
+    <value>resourcemanager</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+  </property>
+  <property>
     <name>yarn.resourcemanager.webapp.address</name>
     <value>127.0.0.1:50070</value>
   </property>
 EOF
 sudo sed -i '/<configuration>/r yarn-site-customize' yarn-site.xml
+
+sudo cat >> mapred-site-customize << EOF
+  <property>
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+  </property>
+  <property>
+    <name>mapreduce.jobhistory.webapp.address</name>
+    <value>127.0.0.1:19888</value>
+  </property>
+EOF
+sudo sed -i '/<configuration>/r mapred-site-customize' mapred-site.xml
