fc2e2ae9f482385bc6241006d4ae3bfa84347a01
MengJingsong <MengJS.92@gmail.com>
2023-11-16 14:44:18 -0500

    Update project_init.sh

--- project_init.sh
+++ project_init.sh
@@ -4,6 +4,6 @@ cd /users/jason92
 mkdir projects
 cd projects
 git clone https://github.com/MengJingsong/hdfs.git
-cd hdfs
-mkdir xs-files
+# cd hdfs
+# mkdir xs-files
 # bash script.sh local-files 0 1000000
