fd5179a82ac872f5f04eee7a74739797834e0da4
MengJingsong <MengJS.92@gmail.com>
2024-01-12 12:31:45 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -60,9 +60,9 @@ def Config( name, public, phystype, raw):
     if phystype != "":
         node.hardware_type = phystype
     node.disk_image = IMAGE
-    # if raw:
-    node.addService(RSpec.Install( SETUP, "/tmp"))
-    node.addService(RSpec.Execute( "sh", "sudo bash /local/repository/init.sh {}".format(params.ver)))
+    if raw:
+        node.addService(RSpec.Install( SETUP, "/tmp"))
+        node.addService(RSpec.Execute( "sh", "sudo bash /local/repository/init.sh {}".format(params.ver)))
     iface = node.addInterface("if0")
     lan.addInterface(iface)
     rspec.addResource(node)
