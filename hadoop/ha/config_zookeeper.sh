abde66a5c2883bbad294577e4b6b4986ceac6b22
MengJingsong <MengJS.92@gmail.com>
2024-01-30 16:02:10 -0500

    Update config_zookeeper.sh

--- hadoop/ha/config_zookeeper.sh
+++ hadoop/ha/config_zookeeper.sh
@@ -10,3 +10,16 @@ EOF
 cd /users/jason92/apache-zookeeper-3.9.1-bin/conf
 
 cp zoo_sample.cfg zoo.cfg
+sed -i -e 's@^dataDir.*@dataDir=/users/jason92/zookeeper@' zoo.cfg
+
+cat >> zoo.cfg << EOF
+server.1=jn1:2888:3888
+server.2=jn2:2888:3888
+server.3=jn3:2888:3888
+EOF
+
+mkdir /users/jason92/zookeeper
+cd /users/jason92/zookeeper
+cat >> myid << EOF
+$1
+EOF
