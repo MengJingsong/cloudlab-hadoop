e843ba0533e558978b1be0f40588f9478944a840
MengJingsong <MengJS.92@gmail.com>
2024-02-01 13:05:17 -0500

    Update config_zookeeper.sh

--- hadoop/ha/config_zookeeper.sh
+++ hadoop/ha/config_zookeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv /tmp/apache-zookeeper-3.9.1-bin /users/jason92
+# mv /tmp/apache-zookeeper-3.9.1-bin /users/jason92
 
 cat >> /users/jason92/.bashrc << EOF
 export ZOOKEEPER=/users/jason92/apache-zookeeper-3.9.1-bin
